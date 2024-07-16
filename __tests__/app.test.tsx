85144158a2b081f07b239a680615913104d0cce8
Ignacio Castillejo Gómez <i.castillejogomez@gmail.com>
2024-07-16 09:36:57 +0200

    test: Improve render test

--- __tests__/app.test.tsx
+++ __tests__/app.test.tsx
@@ -1,15 +1,16 @@
 import 'react-native'
+
 import { renderRouter, screen } from 'expo-router/testing-library'
 
 // Note: import explicitly to use the types shipped with jest.
 import { it, describe } from '@jest/globals'
 
 describe('When rendering the app', () => {
-  it('should render the app', () => {
-    renderRouter(['index'], {
-      initialUrl: '/',
-    })
+  it('should render the app', async () => {
+    const app = renderRouter()
 
     expect(screen).toHavePathname('/')
+    app.getByText('Step One')
+    expect(screen).toMatchSnapshot()
   })
 })
